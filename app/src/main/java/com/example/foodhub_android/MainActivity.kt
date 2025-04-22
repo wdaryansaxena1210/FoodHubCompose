9b36ba65a027808dd8ebdf13e9682223b72db235
Aryan Saxena <aryansaxena1210@gmail.com>
2025-04-21 22:51:41 -0500

    Bug fixes to sign-up screen

--- app/src/main/java/com/example/foodhub_android/MainActivity.kt
+++ app/src/main/java/com/example/foodhub_android/MainActivity.kt
@@ -59,11 +59,3 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FoodHubAndroidTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/com/example/foodhub_android/ui/features/auth/AuthScreen.kt
@@ -0,0 +1,198 @@
+package com.example.foodhub_android.ui.features.auth
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.foodhub_android.R
+import com.example.foodhub_android.ui.theme.Orange
+
+@Composable
+fun AuthScreen() {
+
+    val imageSize = remember {
+        mutableStateOf(IntSize.Zero)
+    }
+
+    val brush = Brush.verticalGradient(
+        colors = listOf(
+            Color.Transparent,
+            Color.Black
+        ),
+        startY = imageSize.value.height.toFloat() / 3,
+        endY = 3000f
+    )
+
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+    ) {
+
+        Image(
+            painter = painterResource(R.drawable.background),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxSize()
+                .onGloballyPositioned {
+                    imageSize.value = it.size
+                }
+                .alpha(0.6f),
+            contentScale = ContentScale.FillBounds,
+        )
+        Box(
+            modifier = Modifier
+                .matchParentSize()
+                .background(brush = brush)
+        )
+        Button(
+            onClick = {},
+            colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .padding(8.dp)
+        ) {
+            Text(
+                text = stringResource(id = R.string.skip),
+                color = Orange
+            )
+        }
+
+        //Column for everything ABOVE buttons (NOT including buttons)
+        Column(
+            modifier = Modifier
+                .background(Color.Transparent)
+                .fillMaxWidth()
+                .padding(top = 110.dp)
+                .padding(16.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.welcome),
+                color = Color.Black,
+                fontSize = 50.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = stringResource(R.string.food_hub),
+                color = Orange,
+                fontSize = 50.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            Text(
+                text = stringResource(R.string.food_hub_desc),
+                color = Color.DarkGray,
+                fontSize = 20.sp,
+                modifier = Modifier.padding(vertical = 16.dp)
+            )
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+                .padding(16.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Button(
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Row(
+                        modifier = Modifier.padding(8.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_google),
+                            contentDescription = null,
+                            modifier = Modifier.size(24.dp)
+                        )
+                        Spacer(modifier = Modifier.padding(4.dp))
+                        Text(
+                            text = stringResource(R.string.sign_with_google),
+                            color = Color.Black,
+                            fontSize = 20.sp
+                        )
+                    }
+                }
+                Spacer(modifier = Modifier.padding(4.dp))
+                Button(
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Row(
+                        modifier = Modifier.padding(8.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_facebook),
+                            contentDescription = null,
+                            modifier = Modifier.size(24.dp)
+                        )
+                        Spacer(modifier = Modifier.padding(4.dp))
+                        Text(
+                            text = stringResource(R.string.sign_with_facebook),
+                            color = Color.Black,
+                            fontSize = 20.sp
+                        )
+                    }
+                }
+            }
+            Spacer(modifier = Modifier.padding(4.dp))
+            Button(
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Gray.copy(alpha = 0.2f)),
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(32.dp),
+                border = BorderStroke(1.dp, Color.White)
+            ) {
+
+                Text(text = stringResource(R.string.sign_with_email), color = Color.White)
+            }
+            TextButton(onClick = {}) {
+                Text(text = stringResource(R.string.aleady_have_account), color = Color.White)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun AuthScreenPreview() {
+    AuthScreen()
+}
