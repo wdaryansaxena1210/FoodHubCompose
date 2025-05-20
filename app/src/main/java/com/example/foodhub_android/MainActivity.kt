8a886276a0d0090dcebf475e997f1c5dcde4ed60
Aryan Saxena <aryansaxena1210@gmail.com>
2025-05-20 12:56:00 -0500

    Add Oauth and final touches with safeApi

--- app/src/main/java/com/example/foodhub_android/MainActivity.kt
+++ app/src/main/java/com/example/foodhub_android/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.foodhub_android
 
-import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,16 +9,11 @@ import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.animation.doOnEnd
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -35,12 +27,6 @@ import com.example.foodhub_android.ui.navigation.Login
 import com.example.foodhub_android.ui.navigation.SignUp
 import com.example.foodhub_android.ui.theme.FoodHubAndroidTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
 import javax.inject.Inject
 
 @AndroidEntryPoint
---
+++ app/src/main/java/com/example/foodhub_android/ui/features/home/HomeScreen.kt
@@ -0,0 +1,10 @@
+package com.example.foodhub_android.ui.features.home
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@Composable
+fun HomeScreen(navController: NavController ) {
+
+}
